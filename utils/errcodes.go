9841f572f800b4f090e4a382782c7e211fbcca23
Pomlon <usmcinu@gmail.com>
2018-12-27 17:45:16 +0100

    missing conditions error codes added

--- utils/errcodes.go
+++ utils/errcodes.go
@@ -3,7 +3,11 @@ package errcodes
 type ErrCode int
 
 const (
-	OK            ErrCode = 0
-	JSONParseErr  ErrCode = 1
-	UnkownCommand ErrCode = 2
+	OK              ErrCode = 0
+	JSONParseErr    ErrCode = 1
+	UnkownCommand   ErrCode = 2
+	AlreadyInside   ErrCode = 3
+	UnfulfilledReqs ErrCode = 4
+	CannotInTown    ErrCode = 5
+	DoesNotExist    ErrCode = 6
 )
