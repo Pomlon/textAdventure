69bbf66c96caa9040726751871a55d77f356a660
Pomlon <usmcinu@gmail.com>
2018-12-27 22:44:28 +0100

    added monsters still live, newturn and outofresource errors

--- utils/errcodes.go
+++ utils/errcodes.go
@@ -3,11 +3,14 @@ package errcodes
 type ErrCode int
 
 const (
-	OK              ErrCode = 0
-	JSONParseErr    ErrCode = 1
-	UnkownCommand   ErrCode = 2
-	AlreadyInside   ErrCode = 3
-	UnfulfilledReqs ErrCode = 4
-	CannotInTown    ErrCode = 5
-	DoesNotExist    ErrCode = 6
+	OK                ErrCode = 0
+	JSONParseErr      ErrCode = 1
+	UnkownCommand     ErrCode = 2
+	AlreadyInside     ErrCode = 3
+	UnfulfilledReqs   ErrCode = 4
+	CannotInTown      ErrCode = 5
+	DoesNotExist      ErrCode = 6
+	OutOfResource     ErrCode = 7
+	NewTurn           ErrCode = 8
+	MonstersStillLive ErrCode = 9
 )
