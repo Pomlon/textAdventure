18d052133c6caf7d003ff1bd44285206eca9483f
Pomlon <usmcinu@gmail.com>
2018-12-27 22:45:16 +0100

    implemented getHit, get first living

--- pawn.go
+++ pawn.go
@@ -1,21 +1,33 @@
 package main
 
-type pawn struct {
-	hp   int
-	mana int
-	atk  int
-	def  int
+type Pawn struct {
+	Hp     int
+	Mana   int
+	Atk    int
+	Def    int
+	Dead   bool
+	Action bool
 }
 
-type IFpawn interface {
-	getHit()
+type IFPawn interface {
+	getHit(amnt int)
 	die()
 }
 
-func (h *pawn) getHit() {
-
+func (p *Pawn) getHit(amnt int) int {
+	var hitAfterCalcs int
+	hitAfterCalcs = amnt - p.Def
+	if hitAfterCalcs <= 0 {
+		hitAfterCalcs = 1
+	}
+	p.Hp -= hitAfterCalcs
+	if p.Hp <= 0 {
+		p.die()
+	}
+	return hitAfterCalcs
 }
 
-func (h *pawn) die() {
-
+func (p *Pawn) die() {
+	p.Dead = true
+	//todo drop items prolly
 }
